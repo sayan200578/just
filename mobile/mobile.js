6323b79c6ea5fde431d37a5f6b31b8ad3c0491ed
sayan200578 <kumarsayan2005@gmail.com>
2025-11-30 01:10:46 -0800

    I update these all

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -1,7 +1,7 @@
 let Samsung = {
   name: "Samsung Galaxy S25 Ultra",
   price: 150000,
-  features: ["5G", "128GB Storage", "8GB RAM", "Triple Camera"],
+  features: ["5G", "128GB Storage", "8GB RAM", "Triple Camera", "Includes S-Pen"],
   rating: 4.8,
 };
 
@@ -10,7 +10,7 @@ document.querySelector("#about").innerHTML =` ${Samsung.name}
  <p> Price: ₹ ${Samsung.price} <p>
  Features: ${Samsung.features.join(", ")}<p>
  Rating: ${Samsung.rating} ⭐`;
-}
+}   
 
 let vivo = {
   name: "Vivo V50 Pro",
@@ -81,11 +81,137 @@ document.querySelector("#about5").innerHTML =` ${SamsungFold.name}
 }
 let countItems = 0;
 
-function nowCartItems(countItems) {
+let AsusRogPhone9pro = {
+name: "Asus ROG Phone 9 Pro",
+price: 152606,
+features: ["5G", "1TB Storage", "16GB RAM", "Triple Camera"],
+rating: 4.6,
+}
+
+function displayMobileDetailsAsusRogPhone9pro() {
+document.querySelector("#about6").innerHTML =` ${AsusRogPhone9pro.name}
+  <p> Price: ₹ ${AsusRogPhone9pro.price} <p>
+  Features: ${AsusRogPhone9pro.features.join(", ")}<p>
+  Rating: ${AsusRogPhone9pro.rating} ⭐`;
+}
+
+let googlepixel10pro = {
+name: "Google Pixel 10 Pro",
+price: 129999,
+features: ["5G", "512GB Storage", "16GB RAM", "Quad Camera"],
+rating: 4.8,
+}
+
+function displayMobileDetailsGooglePixel10pro() {
+document.querySelector("#about7").innerHTML =` ${googlepixel10pro.name}
+  <p> Price: ₹ ${googlepixel10pro.price} <p>
+  Features: ${googlepixel10pro.features.join(", ")}<p>
+  Rating: ${googlepixel10pro.rating} ⭐`;
+}
+
+let InfinixZeroflip5G = {
+name: "Infinix Zero Flip 5G",
+price: 39999,
+features: ["5G", "256GB Storage", "8GB RAM", "Dual Camera"],
+rating: 3.9,
+}
+
+function displayMobileDetailsInfinixZeroflip5G() {
+document.querySelector("#about8").innerHTML =` ${InfinixZeroflip5G.name}
+  <p> Price: ₹ ${InfinixZeroflip5G.price} <p>
+  Features: ${InfinixZeroflip5G.features.join(", ")}<p>
+  Rating: ${InfinixZeroflip5G.rating} ⭐`;
+}
+
+let iphone17promax  = {
+name: "iPhone 17 Pro Max",
+price: 229999,
+features: ["5G", "2TB Storage", "16GB RAM", "Triple Camera"],
+rating: 4.5,
+}
+
+function displayMobileDetailsIphone17promax() {
+document.querySelector("#about9").innerHTML =` ${iphone17promax.name}
+  <p> Price: ₹ ${iphone17promax.price} <p>
+  Features: ${iphone17promax.features.join(", ")}<p>
+  Rating: ${iphone17promax.rating} ⭐`;
+}
+
+let iqoo15pro  = {
+name: "iQOO 15 Pro",
+price: 79999,
+features: ["5G", "512GB Storage", "12GB RAM", "Triple Camera"],
+rating: 4.0,
+}
+
+function displayMobileDetailsIqoo15pro() {
+document.querySelector("#about10").innerHTML =` ${iqoo15pro.name}
+  <p> Price: ₹ ${iqoo15pro.price} <p>
+  Features: ${iqoo15pro.features.join(", ")}<p>
+  Rating: ${iqoo15pro.rating} ⭐`;
+}
+
+let motorolarazr60ultra  = {
+name: "Motorola Razr 60 Ultra",
+price: 109999,
+features: ["5G", "512GB Storage", "12GB RAM", "Dual Camera"],
+rating: 4.9,
+}
+
+function displayMobileDetailsMotorolaRazr60ultra() {
+document.querySelector("#about11").innerHTML =` ${motorolarazr60ultra.name}
+  <p> Price: ₹ ${motorolarazr60ultra.price} <p>
+  Features: ${motorolarazr60ultra.features.join(", ")}<p>
+  Rating: ${motorolarazr60ultra.rating} ⭐`;
+}
+
+let samsungs24ultra = {
+name: "Samsung Galaxy S24 Ultra",
+price: 120000,
+features: ["5G", "256GB Storage", "12GB RAM", "Quad Camera","Includes S-Pen"],
+rating: 4.5,
+}
+
+function displayMobileDetailsSamsungs24ultra() {
+document.querySelector("#about12").innerHTML =` ${samsungs24ultra.name}
+  <p> Price: ₹ ${samsungs24ultra.price} <p>
+  Features: ${samsungs24ultra.features.join(", ")}<p>
+  Rating: ${samsungs24ultra.rating} ⭐`;
+}
+
+let NothingPhone3  = {
+name: "Nothing Phone 3(White)",
+price: 69999,
+features: ["5G", "256GB Storage", "12GB RAM", "Dual Camera"],
+rating: 4.2,
+}
+
+function displayMobileDetailsNothingPhone3() {
+document.querySelector("#about13").innerHTML =` ${NothingPhone3.name}
+  <p> Price: ₹ ${NothingPhone3.price} <p>
+  Features: ${NothingPhone3.features.join(", ")}<p>
+  Rating: ${NothingPhone3.rating} ⭐`;
+}
+
+let sonyXperia1VII  = {
+name: "Sony Xperia 1 VII",
+price: 119999,
+features: ["5G", "512GB Storage", "12GB RAM", "Triple Camera"],
+rating: 4.4,
+}
+
+function displayMobileDetailsSonyXperia1VII() {
+document.querySelector("#about14").innerHTML =` ${sonyXperia1VII.name}
+  <p> Price: ₹ ${sonyXperia1VII.price} <p>
+  Features: ${sonyXperia1VII.features.join(", ")}<p>
+  Rating: ${sonyXperia1VII.rating} ⭐`;
+}
+
+/*function nowCartItems(countItems) {
   if(countItems >= 0) { 
 return alert (`Your cart has ${countItems} items.`); 
 }
-}
+}*/
 
 function displayMobileDetails()
 {
@@ -97,6 +223,15 @@ function displayMobileDetails()
     displayMobileDetailsGoogle(); 
     displayMobileDetailsOppo();
     displayMobileDetailsSamsungFold();
+    displayMobileDetailsAsusRogPhone9pro();
+    displayMobileDetailsGooglePixel10pro();
+    displayMobileDetailsInfinixZeroflip5G();
+    displayMobileDetailsIphone17promax();
+    displayMobileDetailsIqoo15pro();
+    displayMobileDetailsMotorolaRazr60ultra();
+    displayMobileDetailsSamsungs24ultra();
+    displayMobileDetailsNothingPhone3();
+    displayMobileDetailsSonyXperia1VII();
   }
   return display();
 }
